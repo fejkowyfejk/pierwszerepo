9c5ae01f267cdd0272226ed26329983fde0a0ee4
student <student@WNSINOZ.KUL.PL>
2022-03-28 16:51:05 +0200

    Commit Bartosz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("szybki test");
         System.out.println("Slack Jakub");
-        //test
+        System.out.println("Slack Bartosz");
     }
 }
